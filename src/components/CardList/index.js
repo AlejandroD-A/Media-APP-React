d6fbce041e01f3f1a0dc2cf7c785779eabee8e8c
AleDA <alejandrodaliaga@gmail.com>
2021-05-09 12:26:18 -0300

    Se agrego masonry en el shortList

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 import { CardList as CardListStyled } from './styles'
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 
 function CardList({ children }) {
-  return <CardListStyled>{children}</CardListStyled>
+  return (
+    <CardListStyled>
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2 }}>
+        <Masonry>{children}</Masonry>
+      </ResponsiveMasonry>
+    </CardListStyled>
+  )
 }
 
 export default React.memo(CardList)
